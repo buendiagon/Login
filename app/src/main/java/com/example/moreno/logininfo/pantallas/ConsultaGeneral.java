704bfaa76a0e7aa685ac1f28d24b69ad6e6d8563
Moreno <you@example.com>
2018-09-13 15:47:21 -0500

    listo

---
+++ app/src/main/java/com/example/moreno/logininfo/pantallas/ConsultaGeneral.java
@@ -0,0 +1,15 @@
+package com.example.moreno.logininfo.pantallas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.moreno.logininfo.R;
+
+public class ConsultaGeneral extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta_general);
+    }
+}
