0596f6796b8b55682add98142a4b039195c0d30e
Moreno <you@example.com>
2018-09-13 19:37:57 -0500

    listo

--- app/src/main/java/com/example/moreno/logininfo/Base_Datos/BaseDatos.java
+++ app/src/main/java/com/example/moreno/logininfo/Base_Datos/BaseDatos.java
@@ -1,5 +1,6 @@
 package com.example.moreno.logininfo.Base_Datos;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -11,16 +12,45 @@ public class BaseDatos extends SQLiteOpenHelper{
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+    public void onCreate(SQLiteDatabase db) {
 
+        db.execSQL("create table tb_usuarios(documento Integer Primary key,nombre text,fecha text,email,text,user text)");
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+
+        db.execSQL("drop table if exists tb_usuarios");
+        db.execSQL("create table tb_usuarios(documento Integer Primary key,nombre text,fecha text,email,text,user text)");
 
     }
 
+    //crear 5 metodos
 
+    public  void insertar(Context context,ContentValues registro){
+        BaseDatos baseDatos= new BaseDatos(context,"baseDatos",null, 1);
+        SQLiteDatabase db = baseDatos.getWritableDatabase();
+        db.insert("tb_usuarios",null, registro);
+
+    }
+
+    public  void modificar(Context context){
+        BaseDatos baseDatos= new BaseDatos(context,"baseDatos",null, 1);
+        SQLiteDatabase db = baseDatos.getWritableDatabase();
+        
+    }
+
+    public  void eliminar(){
+
+    }
+
+    public  void consulta(){
+
+    }
+
+    public  void insertarGeneral(){
+
+    }
 
 
 
